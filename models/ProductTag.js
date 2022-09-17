60a88629e08c3057ab05cd0dda5bf4e2f2d847d3
Levi H <levi.heintzelman@gmail.com>
2022-09-17 14:21:21 -0600

    Fixed product tags model. Seeding appears to work

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,20 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    product_id: {
+    id: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
       references: {
         model: 'product',
         key: 'id',
+        unique: 'false',
       },
     },
     tag_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
         model: 'tag',
         key: 'id',
+        unique: 'false',
       },
     },
   },
